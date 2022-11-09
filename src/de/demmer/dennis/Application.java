3d19a16145d99c9f12ef2e907342f36ec5c379b4
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2022-11-09 19:23:26 +0100

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -2,6 +2,90 @@ package de.demmer.dennis;
 
 public class Application {
 
+	
+	public static void main(String[] args) {
+		
+		
+//		checkEvenWhile(100);
+//		checkEvenFor(100);
+		
+
+		//-----------------------------------------------
+		
+		//      0         1        2         3
+		// [  "Hallo" | "Welt" | "Test" | "Java" ]
+	
+//		String[] stringArray = {"Hallo", "Welt", "Test", "Java"};
+		
+		
+		//-----------------------------------------------
+		
+		//      0      1        2       3
+		// [ "Test1" | ----- | ----- | ----- ]
+		String[] stringArray = new String[4];
+		
+		stringArray[0] = "Test1";
+
+
+		//-----------------------------------------------
+		
+		
+		char[]  zeichenkette = {'T', 'e', 's', 't', '1'};
+		
+		String test1 = "Test1";
+		
+		System.out.println(test1.charAt(0));
+		
+		
+		
+		
+		// --------- iterieren über ein Array mit einer for-schleife -------------
+		
+		for(int i = 0; i < stringArray.length ; i++) {
+			System.out.println(stringArray[i]);
+		}
+		
+		
+		
+		
+		
+	
+	}
+	
+	
+	
+	static void checkEvenWhile(int length) {
+		
+		int counter = 0;
+		while(counter <= length) {
+
+			if(counter % 2 == 0) {
+				System.out.println(counter);
+			}
+			
+//			counter++;
+			counter = counter + 1;
+		}
+	}
+	
+	
+	static void checkEvenFor(int length) {
+		
+
+			// counter erstellen | Abbruchbedingung    |    counter verändern
+		for(int counter = 0      ; counter <= length   ;  counter++          ) {
+
+			if(counter % 2 == 0) {
+				System.out.println(counter);				
+			}
+
+		}	
+		
+	}
+	
+	
+	
+	
 
 	
 	
